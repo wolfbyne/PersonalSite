f30577a1072e480f53cee3280dcef065e3158c64
Gordon Fleming <flemingrgordon@gmail.com>
2022-12-01 22:01:57 +0200

    complete redo

--- src/routes/index.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-    <title>Gordon Fleming</title>
-</svelte:head>
-
-<h1>Welcome to my site</h1>
-<p>Visit <a target="_blank" href="https://splyce.dev">splyce.dev</a> to read more about what I do...</p>
